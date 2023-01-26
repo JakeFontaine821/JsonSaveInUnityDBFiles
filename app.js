04bcd0b097cf9f2347be0a0210cc5074db081819
Jake Fontaine <72557178+JakeFontaine821@users.noreply.github.com>
2023-01-26 15:53:55 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,51 @@
+var express = require("express");
+var app = express();
+var path = require("path");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var port = process.env.port||3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.json());
+
+mongoose.connect("mongodb://127.0.0.1:27017/gameEntries",{
+    useNewURLParser:true
+}).then(function(){
+    console.log("Connected to MongoDB Database");
+}).catch(function(err){
+    console.log(err);
+});
+
+require("./modules/Game");
+var Game = mongoose.model("game");
+
+//example routes
+app.get("/", function(req, res){
+    res.send("Hello There");
+})
+
+app.get("/poop", function(req, res){
+    res.send("AH PPOOOOP NOOO");
+})
+
+app.post("/saveGame", function(req, res){
+    console.log(req.body);
+
+    new Game(req.body).save().then(function(){
+        //res.send(req.body); 
+        res.redirect("index.html");
+    });
+})
+
+app.get("/getGames", function(req, res){
+    Game.find({}).then(function(game){
+        console.log({game});
+        res.json(game);
+    })
+})
+
+app.use(express.static(__dirname+"/pages"));
+app.listen(port, function(){
+    console.log(`Running on port ${port}`);
+})
